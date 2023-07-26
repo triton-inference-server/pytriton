330d7484e92303255769332101a132174ae458fb
Piotr Marcinkiewicz <piotrm@nvidia.com>
2023-07-26 05:23:16 -0700

    Add support for custom docker platform

--- scripts/add_libs_to_wheel.sh
+++ scripts/add_libs_to_wheel.sh
@@ -16,7 +16,6 @@
 set -x
 set -e
 
-TARGET_PLATFORM=manylinux_2_31_x86_64
 SCRIPTS_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
 
 TRITON_DOCKER_IMAGE="${1}"
@@ -24,7 +23,12 @@ TRITON_LOCAL_DIR=$(realpath "${2}")
 WHEEL_PATH=$(realpath "${3}")
 DIST_DIR="$(dirname "${WHEEL_PATH}")"
 
-DOCKER_CONTAINER_ID=$(docker create --rm -w "${PWD}" "${TRITON_DOCKER_IMAGE}" bash -c "sleep 1h")
+DOCKER_PLATFORM=${4}
+# get arch from DOCKER_PLATFORM
+ARCH=$(echo "${DOCKER_PLATFORM}" | cut -d'/' -f2)
+WHEEL_PLATFORM=manylinux_2_31_${ARCH}
+
+DOCKER_CONTAINER_ID=$(docker create --rm --pull always --platform ${DOCKER_PLATFORM} -w "${PWD}" "${TRITON_DOCKER_IMAGE}" bash -c "sleep 1h")
 docker start "${DOCKER_CONTAINER_ID}"
 
 docker exec "${DOCKER_CONTAINER_ID}" mkdir -p "${DIST_DIR}"
@@ -35,7 +39,7 @@ docker cp "${TRITON_LOCAL_DIR}" "${DOCKER_CONTAINER_ID}:${TRITON_LOCAL_DIR}"
 
 docker exec "${DOCKER_CONTAINER_ID}" pip install auditwheel==5.3.0 patchelf==0.17.2
 docker cp "${SCRIPTS_DIR}/auditwheel_patched.py" "${DOCKER_CONTAINER_ID}:/tmp/"
-docker exec "${DOCKER_CONTAINER_ID}" bash -c "LD_LIBRARY_PATH=${TRITON_LOCAL_DIR}/external_libs /tmp/auditwheel_patched.py -vvvv repair --plat ${TARGET_PLATFORM} ${WHEEL_PATH}"
+docker exec "${DOCKER_CONTAINER_ID}" bash -c "LD_LIBRARY_PATH=${TRITON_LOCAL_DIR}/external_libs /tmp/auditwheel_patched.py -vvvv repair --plat ${WHEEL_PLATFORM} ${WHEEL_PATH}"
 
 WHEELHOUSE_DIR="$(dirname "${DIST_DIR}")/wheelhouse"
 RESULT_WHEEL_PATH=$(docker exec "${DOCKER_CONTAINER_ID}" bash -c "find ${WHEELHOUSE_DIR} -type f -name *.whl")
